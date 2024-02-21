0504e905ec2f43f82aa67f5a90508c0bb25e3a32
Santino <santinobertola03@gmail.com>
2024-02-21 00:58:10 -0300

    rutas

---
+++ front/src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { Routes as ReactDomRoutes, Route } from "react-router-dom"          
+
+
+
+const Routes = () => {
+    return (
+        <ReactDomRoutes>
+            <Route path="/" element={<></>} />
+            <Route path="/login" element={<></>} />
+            <Route path="/register" element={<></>} />
+
+
+
+        </ReactDomRoutes>
+    )
+}
+
+export default Routes;
