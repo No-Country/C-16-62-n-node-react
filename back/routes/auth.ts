a39ab431b5a911268d3ad342859522abbf480a99
pablomh3 <pablonudenberg@gmail.com>
2024-02-22 00:36:13 -0300

    register

--- back/routes/auth.ts
+++ back/routes/auth.ts
@@ -3,15 +3,12 @@ import { registerUser, addWorkerData } from '../controllers/auth';
 import { check } from "express-validator"; 
 import { collectErrors } from "../middlewares/collectErrors"; 
 
-
-
-
 const router = Router ()
 
 router.post("/registerUser", [
     check ("name", "te falto el nombre").not().isEmpty(),
     check ("email", "faltó el email").isEmail(),
-    check ("phone", "te faltó el teléfono").isEmpty(),
+    check ("phone", "te faltó el teléfono").isNumeric(),
     check ("password", "la contraseña debe contener al menos 6 caracteres").isLength({min: 6}),
     check ("location", "te faltó la locación").not().isEmpty(),
 
