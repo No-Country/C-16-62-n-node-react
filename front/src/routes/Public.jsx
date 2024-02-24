f0dc893682ba191b37ab2247731ae45856a52807
Federico Dorigatti Assan <doriass.f@gmail.com>
2024-02-24 00:02:05 -0300

    Creacion de vista register, actualizacion del navbar, componentes cards y filter

--- front/src/routes/Public.jsx
+++ front/src/routes/Public.jsx
@@ -3,6 +3,7 @@ import HomePage from "../pages/homepage/HomePage";
 import Login from "../pages/login/Login"; 
 import Register from "../pages/register/Register";        
 import NotFound from "../pages/notfound/NotFound";
+import FormService from "../pages/services/FormService";
 
 
 
@@ -12,7 +13,8 @@ function Public() {
 
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />       
+        <Route path="/register" element={<Register />} />
+        <Route path="/forms/services" element={<FormService />} />              
         <Route path="*" element={<NotFound />} />
 
       </Routes>
