e2f945b2537503d217cdbcac847b165439011742
Santino <santinobertola03@gmail.com>
2024-02-29 11:25:45 -0300

    Cerrar sesión agregado

--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -32,31 +32,6 @@ function NavBar() {
               >
                 {currentUser.name}
               </Link>{" "}
-              <div className="flex md:order-2">
-                <Dropdown
-                  arrowIcon={false}
-                  inline
-                  label={
-                    <Avatar
-                      alt="User settings"
-                      img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                      rounded
-                    />
-                  }
-                >
-                  <Dropdown.Header>
-                    <span className="block text-sm">{currentUser.name}</span>
-                    <span className="block truncate text-sm font-medium">
-                      {currentUser.email}
-                    </span>
-                  </Dropdown.Header>
-                  <Dropdown.Item>Dashboard</Dropdown.Item>
-                  <Dropdown.Item>Settings</Dropdown.Item>
-                  <Dropdown.Item>Earnings</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item>Cerrar sesión</Dropdown.Item>
-                </Dropdown>
-              </div>
             </>
           ) : (
             <>
--- front/src/pages/profile/Profile.jsx
+++ front/src/pages/profile/Profile.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
+import { useAuth } from "../../context/AuthContext";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 const reviews = [
   {
@@ -22,7 +26,15 @@ const reviews = [
 ];
 
 const Profile = () => {
+  const { currentUser, logout } = useAuth();
+  const [openModal, setOpenModal] = useState(false);
+  const navigate  = useNavigate();
 
+  const handleLogout = () => {
+    logout();
+    setOpenModal(false);
+    navigate("/login");
+  };
 
   return (
     <div className="container mx-auto p-10">
@@ -35,29 +47,35 @@ const Profile = () => {
           />
           <div className="text-left">
             <p className="text-xl text-gray-800 font-bold mb-1">
-              Nombre del Usuario
+            {currentUser ? currentUser.name : 'Nombre de Usuario'}
             </p>
             <p className="text-base text-gray-500 font-normal">
-              Zona de Residencia: ejemplo.
+              Zona de Residencia: {currentUser ? currentUser.location : 'Lugar de residencia'}.
             </p>
             <p className="text-base text-gray-500 font-normal">
-              Contacto: ejemplo@correo.com
+              {currentUser ? currentUser.email : 'Email del usuario'}
             </p>
           </div>
-          <button type="button" class="text-white bg-[#1995AD] hover:bg-[#2aa0b8] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Editar perfil</button>
+          <button
+            type="button"
+            class="text-white bg-[#1995AD] hover:bg-[#2aa0b8] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          >
+            Editar perfil
+          </button>
         </div>
       </div>
 
       <div className="bg-white shadow rounded-lg p-6 mt-5">
         <h2 className="text-xl font-semibold text-gray-800 mb-4">Reseñas</h2>
         <div className="flex flex-wrap -mx-4">
-          {reviews.slice(0, 3).map(review => (
+          {reviews.slice(0, 3).map((review) => (
             <div key={review.id} className="w-full sm:w-1/2 md:w-1/3 px-4 mb-4">
               <div className="bg-gray-100 p-6 rounded-lg h-full">
                 <p className="text-gray-600">"{review.content}"</p>
                 <div className="flex items-center justify-between mt-4">
                   <div className="text-sm text-gray-500">
-                    <span>Por: {review.author}</span>, <span>Fecha: {review.date}</span>
+                    <span>Por: {review.author}</span>,{" "}
+                    <span>Fecha: {review.date}</span>
                   </div>
                 </div>
               </div>
@@ -65,8 +83,47 @@ const Profile = () => {
           ))}
         </div>
         <div className="text-center mt-6">
-        <button type="button" class="text-white bg-[#1995AD] hover:bg-[#2aa0b8] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Ver más reseñas</button>
+          <button
+            type="button"
+            class="text-white bg-[#1995AD] hover:bg-[#2aa0b8] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          >
+            Ver más reseñas
+          </button>
         </div>
+      </div>
+
+      <div className="text-center mt-6">
+        <button
+          type="button"
+          class="text-white bg-[#ad1919] hover:bg-[#be1c1c] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          onClick={() => setOpenModal(true)}
+        >
+          Cerrar sesión
+        </button>
+        <Modal
+          show={openModal}
+          size="md"
+          onClose={() => setOpenModal(false)}
+          popup
+        >
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14" />
+              <h3 className="mb-5 text-lg font-normal">
+                ¿Estás seguro que deseas cerrar sesión?
+              </h3>
+              <div className="flex justify-center gap-4">
+                <Button color="failure" onClick={handleLogout}>
+                  {"Si"}
+                </Button>
+                <Button color="gray" onClick={() => setOpenModal(false)}>
+                  No
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
